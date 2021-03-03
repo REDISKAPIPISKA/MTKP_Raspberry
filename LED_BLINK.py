a9dbbf33c5cd2c8b6d75abc1d3be880e6f8b05fb
Bauman Robotics | BMSTU <baumanrobotics@gmail.com>
2021-03-03 15:23:45 +0300

    new

--- LED_BLINK.py
+++ LED_BLINK.py
@@ -4,14 +4,14 @@ import time
 
 GPIO.setmode(GPIO.BOARD)
 
-GPIO.setup(7, GPIO.OUT)
+GPIO.setup(3, GPIO.OUT)
 
 while True:
 
-____GPIO.output(7, 1)
+    GPIO.output(3, 1)
 
-____time.sleep(1)
+    time.sleep(1)
 
-____GPIO.output(3, 0)
+    GPIO.output(3, 0)
 
-____time.sleep(1)
+    time.sleep(1)
